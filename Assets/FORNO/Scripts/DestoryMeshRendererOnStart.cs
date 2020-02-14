32704d71d248ca29fbf2a81eaebbfb6424f2de94
FORNO <forno@xmaho.link>
2020-02-14 09:24:13 +0900

    Implement Sync Nursecare Data

---
+++ Assets/FORNO/Scripts/DestoryMeshRendererOnStart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryMeshRendererOnStart : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(GetComponent<MeshRenderer>());
+    }
+}
---
+++ Assets/FORNO/Scripts/ECS/Authoring/CopyTransformToGameObjectAuthoring.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class CopyTransformToGameObjectAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponent<CopyTransformToGameObject>(entity);
+    }
+}
