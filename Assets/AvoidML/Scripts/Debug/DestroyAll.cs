93d6ca863b8b5bbafff95ace23ea66623d569821
FORNO <forno@xmaho.link>
2020-02-24 22:42:54 +0900

    Add model view with UnityChan

---
+++ Assets/AvoidML/Scripts/Debug/DestroyAll.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject);
+    }
+}
